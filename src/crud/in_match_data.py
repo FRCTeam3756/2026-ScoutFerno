0aac6d5b19e4fd07cb5ba98ab1710d29efe31fcb
Rainbow2019 <ethanmcauslan@gmail.com>
2026-03-17 19:50:59 -0400

    Fixed spelling for in_match_data file

--- src/crud/in_match_data.py
+++ src/crud/in_match_data.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
 
-from models.in_match_data_class import Match_Data, Match_Data_Create, Match_Data_Update
+from models.in_match_data_class import In_Match_Data, In_Match_Data_Create, In_Match_Data_Update
 from models.sql_models import engine
 from models.fastapi_models import lifespan
 
@@ -10,10 +10,10 @@ from models.fastapi_models import lifespan
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post("/data/match_data/", response_model=Match_Data)
-def create_data(match_data: Match_Data_Create):
+@app.post("/data/match_data/", response_model=In_Match_Data)
+def create_data(match_data: In_Match_Data_Create):
     with Session(engine) as session:
-        db_data = Match_Data.model_validate(match_data)
+        db_data = In_Match_Data.model_validate(match_data)
         session.add(db_data)
 
         try:
@@ -28,29 +28,29 @@ def create_data(match_data: Match_Data_Create):
             )
 
 
-@app.get("/data/match_data/", response_model=list[Match_Data])
+@app.get("/data/match_data/", response_model=list[In_Match_Data])
 def read_match_data():
     with Session(engine) as session:
-        match_data = session.exec(select(Match_Data)).all()
+        match_data = session.exec(select(In_Match_Data)).all()
         return match_data
 
 
-@app.get("/data/match_data/team/{team_number}", response_model=list[Match_Data])
+@app.get("/data/match_data/team/{team_number}", response_model=list[In_Match_Data])
 def read_match_data_by_team(team_number: int):
     with Session(engine) as session:
-        statement = select(Match_Data).where(Match_Data.team_number == team_number)
+        statement = select(In_Match_Data).where(In_Match_Data.team_number == team_number)
         results = session.exec(statement).all()
         if not results:
             raise HTTPException(status_code=404, detail="Team data not found")
         return results
     
 
-@app.patch("/data/match_data/team/{team_number}/match/{match_number}", response_model=Match_Data)
-def update_team_match_data(team_number: int, match_number: int, match_data: Match_Data_Update):
+@app.patch("/data/match_data/team/{team_number}/match/{match_number}", response_model=In_Match_Data)
+def update_team_match_data(team_number: int, match_number: int, match_data: In_Match_Data_Update):
     with Session(engine) as session:
-        statement = select(Match_Data).where(
-            Match_Data.team_number == team_number,
-            Match_Data.match_number == match_number
+        statement = select(In_Match_Data).where(
+            In_Match_Data.team_number == team_number,
+            In_Match_Data.match_number == match_number
         )
 
         db_match = session.exec(statement).first()
@@ -70,9 +70,9 @@ def update_team_match_data(team_number: int, match_number: int, match_data: Matc
 @app.delete("/data/match_data/team/{team_number}/match/{match_number}")
 def delete_team_one_match_data(team_number: int, match_number: int):
     with Session(engine) as session:
-        statement = select(Match_Data).where(
-            Match_Data.team_number == team_number,
-            Match_Data.match_number == match_number
+        statement = select(In_Match_Data).where(
+            In_Match_Data.team_number == team_number,
+            In_Match_Data.match_number == match_number
         )
 
         match_data = session.exec(statement).first()
@@ -87,8 +87,8 @@ def delete_team_one_match_data(team_number: int, match_number: int):
 @app.delete("/data/match_data/team/{team_number}")
 def delete_team_match_data(team_number: int):
     with Session(engine) as session:
-        statement = select(Match_Data).where(
-            Match_Data.team_number == team_number,
+        statement = select(In_Match_Data).where(
+            In_Match_Data.team_number == team_number,
         )
 
         team_match_data = session.exec(statement).all()
--- src/models/in_match_data_class.py
+++ src/models/in_match_data_class.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field,  SQLModel
 from sqlalchemy import UniqueConstraint
 
-class Match_Data_Base(SQLModel):
+class In_Match_Data_Base(SQLModel):
     team_number: int = Field(index=True)
     match_number: int = Field(index=True)
     ball_storage: int = Field(index=True)
@@ -10,18 +10,18 @@ class Match_Data_Base(SQLModel):
 
     
 
-class Match_Data(Match_Data_Base, table=True):
+class In_Match_Data(In_Match_Data_Base, table=True):
     id: int | None = Field(default=None, primary_key=True)
     __table_args__ = (
         UniqueConstraint("team_number", "match_number", name="data_creation_constraint"),
     )
     
 
-class Match_Data_Create(Match_Data_Base):
+class In_Match_Data_Create(In_Match_Data_Base):
     pass
 
 
-class Match_Data_Update(SQLModel):
+class In_Match_Data_Update(SQLModel):
     ball_storage: int | None = None
     drivetrain_type: str | None = None
     balls: int | None = None
